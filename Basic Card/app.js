31597f44f5b14ba568fe1623b14cd0563818da02
kanikamital0606 <kanikamittal0661996@gmail.com>
2020-07-12 14:12:33 +0530

    update

--- Basic Card/app.js
+++ Basic Card/app.js
@@ -2,44 +2,70 @@ new Vue({
     el: "#app",
     data: {
         event: {
-          eventTitle: "Fest",
-          eventDescription: `It's back! the most awaited fest of the year. Loads of
+            eventTitle: "Fest",
+            eventDescription: `It's back! the most awaited fest of the year. Loads of
             fun and excitement jam-packed over three days. Come and let out
             your amazing side......`,
-          timestamp: ""
+            timestamp: ""
         },
         newNameText: '',
-        guestNames : [],
-        
+        guestNames: [],
+        // to overcome submit class problem 
+        formSubmitClass: '',
+        //capacity limit 
+        eventCapacity: 10,
+        //percentage bar 
+        eventCapacityPercentage: 0
+
+
     },
-    created(){
-        setInterval(this.getNow, 1000);
-    },
+    //comment it , now no use 
+    // created(){
+    //setInterval(this.getNow, 1000);
+    // },
     methods: {
-        formSubmitted: function() {
-            console.log(this.newNameText)
-            this.guestNames.push(this.newNameText)
-            this.newNameText = ''
+        formSubmitted: function () {
+
+            //to display name
+            //console.log(this.newNameText)
+
+            //put if condition to limit attendees list
+            if (this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
+                console.log(this.newNameText) //display atendess list
+                this.guestNames.push(this.newNameText) //push names in empty array guestList
+                this.newNameText = '' //after push and diplay in guest list again become empty 
+                this.formSubmitClass = "submitted" //submitted array
+                this.eventCapacityPercentage = this.guestNames.length / (this.eventCapacity / 100)
+                // ^|| to increase percentage bar
+            }
+            // this.guestNames.push(this.newNameText)
+            // this.newNameText = ''
         },
-        getNow: function() {
-            const today = new Date();
-            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            const dateTime = date +' '+ time;
-            this.event.timestamp = dateTime;
+        // getNow: function () {
+        //     const today = new Date();
+        //     const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        //     const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        //     const dateTime = date + ' ' + time;
+        //     this.event.timestamp = dateTime;
+        // },
+        data(){
+            return {
+                value: this.eventCapacityPercentage,
+                max: 100
+            }
+        },
+        randomValue() {
+            this.value = this.guestNames.length / (this.eventCapacity / 100)
+        }
+
+    },
+    //add computed: to sort names alphabetically
+
+
+    computed: {
+        sortNames: function () {
+            return this.guestNames.sort()
         }
     },
-})
-// new vue({
-//     el: "#app",
-//     data:{
-//         event:{
-//             eventDate: "July 9",
-//             eventTitle: "Diwali Party Invitation!!",
-//             eventDescription: `It's back! the most awaited festival of the season. Loads of fun and excitement jam-packed over two days. Come and let out
-//             your fun side......`,
-//           timestamp: ""
-//         },
-//         newNameText: '',
-//     }
-// })
+
+});
